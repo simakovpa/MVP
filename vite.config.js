da373dc00c65cfcfbde6cb370bb866a90aa4ed05
simakovpa <toli4@outlook.com>
2026-02-24 08:45:14 +0700

    rules upd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MVP/", //тут должно быть название вашего репозитория в гитхабе
+  base: "/MVP/", //nochange
 });
